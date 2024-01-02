1f5cf9d7187f140ac863d132dccaa056856e9546
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-01-02 20:54:52 +0000

    Add example for OpenAI Whisper
    Download a video from YouTube, convert it to an audio track,
    run the track through OpenAI Whisper
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ examples/whisper-youtube.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e -x -o pipefail
+
+# Example by Alex Ellis
+
+# Download a video from YouTube, convert it to audio, then
+# transcribe it using Whisper's tiny model. When CUDA and a GPU are
+# available, it'll run significantly faster, otherwise it'll revert to
+# CPU mode.
+
+## Stage 1 - download the video
+
+# Rex Kruger: Should you get a Japanese pull saw?
+TARGET=https://www.youtube.com/watch?v=Q7bL61cSUpw
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Add ffmpeg to convert to mp4 later
+sudo -E apt-get update -qqqy && \
+  time sudo -E apt-get install -qqqy ffmpeg
+
+DL_URL=https://github.com/yt-dlp/yt-dlp/releases/download/2023.11.16/yt-dlp_linux
+
+if [ "$(uname -m)" == "aarch64" ]; then
+  DL_URL="https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_aarch64"
+fi
+
+sudo curl -LSls -o /usr/local/bin/yt-dlp \
+  $DL_URL && \
+  sudo chmod +x /usr/local/bin/yt-dlp
+
+mkdir -p videos
+mkdir -p uploads
+
+time yt-dlp -o "./videos/video.flv" "$TARGET" && \
+  du -h -d 0 ./videos/*
+
+mkdir -p audio
+
+## Stage 2 - convert the video to mp3
+cd videos
+time ffmpeg -i * -b:a 128K -vn ../audio/track.mp3
+cd ../
+
+## Stage 3 Run Whisper
+
+# Install openai-whisper
+
+time pip install -U openai-whisper
+
+# Transcribe using the tiny model
+
+time whisper --language English ./audio/*.mp3 --model tiny > ./uploads/track.txt
+
